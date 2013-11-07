39da310f2318e9c2c43693f9de3724f0ac808553
Florian Haamann <hafl1016(t)hs-karlsruhe.de>
2013-11-07 18:30:30 +0100

    Position package artikel imported

--- shop/src/main/java/de/shop/bestellverwaltung/domain/Position.java
+++ shop/src/main/java/de/shop/bestellverwaltung/domain/Position.java
@@ -2,13 +2,16 @@ package de.shop.bestellverwaltung.domain;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import de.shop.artikelverwaltung.domain.AbstractArtikel;
+import de.shop.kundenverwaltung.domain.*;
+import de.shop.kundenverwaltung.domain.AbstractKunde;
+import artikelverwaltung.domain.*;
+
 
 @XmlRootElement
 public class Position {
         private int id;
         private int bestellid;
-        private AbstractArtikel artikel;
+        private Artikel artikel;
         private int anzahl;
         public int getId() {
                 return id;
@@ -22,10 +25,10 @@ public class Position {
         public void setBestellid(int bestellid) {
                 this.bestellid = bestellid;
         }
-        public AbstractArtikel getArtikel() {
+        public Artikel getArtikel() {
                 return artikel;
         }
-        public void setArtikel(AbstractArtikel artikel) {
+        public void setArtikel(Artikel artikel) {
                 this.artikel = artikel;
         }
         public int getAnzahl() {
@@ -37,41 +40,6 @@ public class Position {
         public Position() {
                 super();
         }
-        @Override
-        public String toString() {
-                return "Position [id=" + id + ", bestellid=" + bestellid + ", artikel="
-                                + artikel + ", anzahl=" + anzahl + "]";
-        }
-        @Override
-        public int hashCode() {
-                final int prime = 31;
-                int result = 1;
-                result = prime * result + anzahl;
-                result = prime * result + ((artikel == null) ? 0 : artikel.hashCode());
-                result = prime * result + bestellid;
-                result = prime * result + id;
-                return result;
-        }
-        @Override
-        public boolean equals(Object obj) {
-                if (this == obj)
-                        return true;
-                if (obj == null)
-                        return false;
-                if (getClass() != obj.getClass())
-                        return false;
-                Position other = (Position) obj;
-                if (anzahl != other.anzahl)
-                        return false;
-                if (artikel == null) {
-                        if (other.artikel != null)
-                                return false;
-                } else if (!artikel.equals(other.artikel))
-                        return false;
-                if (bestellid != other.bestellid)
-                        return false;
-                if (id != other.id)
-                        return false;
-                return true;
-        }
+
+
 }
