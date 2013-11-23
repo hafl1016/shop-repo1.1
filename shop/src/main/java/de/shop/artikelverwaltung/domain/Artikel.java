94bc11d6a0bce50eba3f17a594f3183504f10d1c
Marius Faßbender <Marius@MARIUS-NOTEBOOK>
2013-11-23 15:16:26 +0100

    Artikel.class überarbeitet

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -2,24 +2,30 @@ package de.shop.artikelverwaltung.domain;
 
 public class Artikel {
 
+	
 	private long id;
 	private String bezeichnung;
 	private double preis;
 	public long getId() {
 		return id;
 	}
+	
 	public String getBezeichnung() {
 		return bezeichnung;
 	}
+	
 	public double getPreis() {
 		return preis;
 	}
+	
 	public void setId(long id) {
 		this.id = id;
 	}
+	
 	public void setBezeichnung(String bezeichnung) {
 		this.bezeichnung = bezeichnung;
 	}
+	
 	public void setPreis(double preis) {
 		this.preis = preis;
 	}
