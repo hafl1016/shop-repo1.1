ce44c7f25c8df834ed4a4b1c92d3bb6c773e43c5
Christian Blust <blch1013@hs-karlsruhe.de>
2013-12-05 12:54:32 +0100

    ArtikelService

--- shop/src/main/java/de/shop/artikelverwaltung/service/ArtikelService.java
+++ shop/src/main/java/de/shop/artikelverwaltung/service/ArtikelService.java
@@ -1,5 +1,26 @@
 package de.shop.artikelverwaltung.service;
 
-public class ArtikelService {
+import java.io.Serializable;
 
+import javax.enterprise.context.Dependent;
+import javax.validation.constraints.NotNull;
+
+import de.shop.artikelverwaltung.domain.Artikel;
+import de.shop.util.interceptor.Log;
+import de.shop.util.Mock;
+
+/**
+ * @author <a href="mailto:Juergen.Zimmermann@HS-Karlsruhe.de">J&uuml;rgen Zimmermann</a>
+ */
+@Dependent
+@Log
+public class ArtikelService implements Serializable {
+	private static final long serialVersionUID = -5105686816948437276L;
+
+	@NotNull(message = "{artikel.notFound.id}")
+	public static Artikel findArtikelById(int id) {
+		// TODO id pruefen
+		// TODO Datenbanzugriffsschicht statt Mock
+		return Mock.findArtikelById(id);
+	}
 }
