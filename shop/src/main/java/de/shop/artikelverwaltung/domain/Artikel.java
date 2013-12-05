81523608111a6fc2f7a22a65dd2f25faedec2760
Marius Faßbender <Marius@MARIUS-NOTEBOOK>
2013-12-05 12:32:27 +0100

    pattern erweitert bei adresse und artikel

--- shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
+++ shop/src/main/java/de/shop/artikelverwaltung/domain/Artikel.java
@@ -23,7 +23,7 @@ public class Artikel {
 	
 	@NotNull
 	@Size(min = 2, max = 50, message="{artikelverwaltung.artikel.bezeichnung.length}")
-	@Pattern(regexp = "[A-Z\u00C4\u00D6\u00DC][a-z\u00E4\u00F6\u00FC\u00DF]+", 
+	@Pattern(regexp = "[A-Z\u00C4\u00D6\u00DC][-A-Za-z0-9\u00E4\u00F6\u00FC\u00DF\u0020]+", 
 			message = "{artikelverwaltung.artikel.bezeichnung.pattern}")
 	private String bezeichnung;
 	
